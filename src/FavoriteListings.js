746b6bb35dc3e6309b1247930b7a5a77ad2e8df4
mark sauer-utley <mark.utley.713@gmail.com>
2019-08-27 19:35:06 -0400

    finished after time

--- src/FavoriteListings.js
+++ src/FavoriteListings.js
@@ -1,26 +1,40 @@
 function FavoriteListings() {
-  this.listings = [];
+	this.listings = [];
 }
 
 FavoriteListings.prototype.isListingFavorited = function(listing) {
-  for (var i = 0; i < this.listings.length; i++) {
-    if (this.listings[i].listing_id == listing.listing_id) {
-      return true;
-    }
-  }
-  return false;
-}
+	for (var i = 0; i < this.listings.length; i++) {
+		if (this.listings[i].listing_id == listing.listing_id) {
+			return true;
+		}
+	}
+	return false;
+};
 
 FavoriteListings.prototype.addListing = function(listing) {
-  this.listings.push(listing);
+	var alreadyFavorite = this.listings.some(function(favorite) {
+		return favorite.listing_id === listing.listing_id;
+	});
+	if (!alreadyFavorite) {
+		this.listings.push(listing);
+	}
 };
 
 FavoriteListings.prototype.removeListing = function(listing) {
+	var found = false;
+	var i = 0;
 
+	while (!found && i < this.listings.length) {
+		if (this.listings[i].listing_id === listing.listing_id) {
+			found = true;
+			this.listings.splice(i, 1);
+		}
+		i++;
+	}
 };
 
 FavoriteListings.prototype.getFavorites = function() {
-  return this.listings;
-}
+	return this.listings;
+};
 
 module.exports = FavoriteListings;
